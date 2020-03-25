7f91f940f3eb8ee4d2b6ae9a1a2a2f9111754ba8
Brandon Fannin <bfannin13@gmail.com>
2020-03-25 16:45:57 -0400

    refactor some pawn move code using new methods

--- js/rules.js
+++ js/rules.js
@@ -11,20 +11,14 @@ function canPawnMove(from, to){
     }
 
     //we cannot move through other pieces.
-    let pathClear = false;
-
-    const spaceToMove = document.getElementById(to).classList[1];
-    pathClear = spaceToMove == 'empty';
-
+    let pathClear = isEmpty(to);
     if (places == 2) {
         const id = turn == 'w' ? from-8 : from+8;
-        const spaceToMoveThrough = document.getElementById(id).classList[1];
-        pathClear = spaceToMoveThrough == 'empty';
+        pathClear = isEmpty(id);
     }
 
     //we can only move forward. Two OR One at start, otherwise One.
     let moveValid = false;
-
     if (places == 1) {
         moveValid = diff == 8;
     }else if (places == 2) {
@@ -33,10 +27,8 @@ function canPawnMove(from, to){
 
     //if there is a piece diagnol to us, we can move there.
     let inForKill = false;
-
     if (diff == 7 || diff == 9) {
-        const pieceColor = spaceToMove.substring(spaceToMove.length-2);
-        inForKill = (spaceToMove != 'empty' && pieceColor != turn);
+        inForKill = !isEmpty(to) && !inYourOwnWay(to); 
     }
 
     return (inForKill || (pathClear && moveValid));
@@ -80,7 +72,7 @@ function canRookMove(from, to){
             }
         }
     }
-    
+
     return ( (canGoHorizontal || canGoVertical) && canMove && !inYourOwnWay(to));
 }
 
